21ab1745ead0fda0eea5181633495343d325f02e
ganghyeun <angwanghyeon611@gmail.com>
2026-09-10 16:38:11 +0900

    Update hello world.py

--- dada/hello world.py
+++ dada/hello world.py
@@ -1,11 +1,12 @@
 def print_hello():
     print("hello world")
 
-    def print_ kit():
-        print("kumoh national")
-        institute of technology
+    def add(x, y):
+a = x + y
+return a
+def print_KIT():
+print(“KIT")
+if __name__ == '__main__’:
 
-        if __name == '_main_':
-            print_kit()
-            print_hello()
-            print
+print_KIT()
+print( print_hello(1,2) )
